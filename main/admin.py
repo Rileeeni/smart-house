beb40e636571850460fe89e99a1eb01abb7e83b7
nabievabu67-hash <nabiev.abu67@gmail.com>
2026-04-27 17:57:41 +0600

    debug

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Profile, Telemetry, Scenarios
+from .models import Profile, Telemetry, Scenarios,Device,Room,Home,Category
 
 admin.site.register(Profile)
 admin.site.register(Telemetry)
 admin.site.register(Scenarios)
+admin.site.register(Device)
+admin.site.register(Room)
+admin.site.register(Home)
+admin.site.register(Category)
