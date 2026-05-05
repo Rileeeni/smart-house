e94ab72e1a83cd984d31232cc8451078806d77f6
Rileeeni <Rileeenizer@gmail.com>
2026-05-05 22:20:29 +0600

    validation

--- main/models.py
+++ main/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator, FileExtensionValidator
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
@@ -17,7 +17,18 @@ class Profile(models.Model):
 
 class Home (models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,validators=[
+        MinValueValidator(4)
+    ])
+    def clean(self):
+        if not self.name.strip():
+            raise ValidationError("Название дома не может быть пустым")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name'],
+                                    name='unique_review')
+        ]
 
 class Room(models.Model):
     home = models.ForeignKey("Home", on_delete=models.CASCADE)
@@ -39,26 +50,68 @@ class Room(models.Model):
 class Device(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    secret_key = models.CharField(max_length=255)
-    name = models.CharField(max_length=100)
+    secret_key = models.CharField(max_length=255,validators=[
+        MinValueValidator(12)
+    ])
+    name = models.CharField(max_length=100,validators=[
+        MinValueValidator(4)
+    ])
+    def clean(self):
+        if not self.name.strip():
+            raise ValidationError("Название комнаты не может быть пустым")
+        if not self.secret_key.strip():
+            raise ValidationError("secret_key не может быть пустым")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name'],
+                                    name='unique_review')
+        ]
+
 
 class Telemetry(models.Model):
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
-    temperature = models.FloatField()
-    humidity = models.FloatField()
+    temperature = models.FloatField(validators=[
+    MinValueValidator(-50, message="Температура не может быть ниже -10"),
+    MaxValueValidator(60, message="Температура не может быть выше 40"),
+    ]
+    )
+    humidity = models.FloatField(validators=[
+    MinValueValidator(0, message="Влажность должна быть в диапазоне от 0 до 100"),
+    MaxValueValidator(100, message="Влажность должна быть в диапазоне от 0 до 100"),
+    ]
+    )
     motion = models.BooleanField(default=False)
     smoke = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,validators=[
+        MinValueValidator(4)
+    ])
     def __str__(self):
         return self.name
+    def clean(self):
+        if not self.name.strip():
+            raise ValidationError("Название дома не может быть пустым")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name'],
+                                    name='unique_review')
+        ]
 
 class Scenarios(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     device = ManyToManyField(Device)
-    scenario = models.JSONField()
+    scenario = models.JSONField(validators=[
+        FileExtensionValidator(["json"])
+    ])
+    def clean(self):
+        if not self.scenario.strip():
+            raise ValidationError("Сценарий не может быть пустым")
+        if not self.device.strip():
+            raise ValidationError("Список девайсов не может быть пуст")
 
 
 
